1919e0a1adf9daf175cecd26f85aa92ea4f9b704
Phenomenal <115654413+phenomenal-softwares@users.noreply.github.com>
2023-09-08 19:11:14 +0100

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "dog",
+        hint: "regarded as man's best friend"
+    },
+    {
+        word: "fire",
+        hint: "he can grow, he needs air, but water will kill him"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet in our solar system"
+    },
+    {
+        word: "keyboard",
+        hint: "he has many keys but can't open any door"
+    },
+    {
+        word: "yoruba",
+        hint: "a West-African tribe"
+    },
+    {
+        word: "lamentations",
+        hint: "a book in the Bible"
+    },
+    {
+        word: "davido",
+        hint: "popular African celebrity"
+    },
+    {
+        word: "ios",
+        hint: "iPhone operating system"
+    },
+    {
+        word: "bug",
+        hint: "computer error"
+    },
+    {
+        word: "jagunjagun",
+        hint: "epic Nollywood movie"
+    },
+    {
+        word: "buddha",
+        hint: "a religious founder"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "bishop",
+        hint: "a chess piece"
+    },
+    {
+        word: "meta",
+        hint: "a social media company"
+    },
+    {
+        word: "bush",
+        hint: "past USA president"
+    },
+    {
+        word: "chatgpt",
+        hint: "open-source artificial intelligence"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "windows",
+        hint: "a computer operating system"
+    },
+    {
+        word: "terabyte",
+        hint: "computer storage unit"
+    },
+    {
+        word: "romance",
+        hint: "sweet experience of two lovebirds"
+    },
+    {
+        word: "pain",
+        hint: "everyone avoids it"
+    },
+    {
+        word: "sleep",
+        hint: "period of inactivity"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "latin",
+        hint: "an ancient language"
+    },
+    {
+        word: "dangote",
+        hint: "black business mogul"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "water",
+        hint: "prerequisite for life"
+    },
+    {
+        word: "h20",
+        hint: "chemical symbol of water"
+    },
+    {
+        word: "romeo",
+        hint: "popular romantic male character"
+    },
+    {
+        word: "oxygen",
+        hint: "a life-supporting gas"
+    },
+    {
+        word: "dollar",
+        hint: "hard currency"
+    },
+    {
+        word: "rainbow",
+        hint: "multi-coloured circular arc"
+    },
+    {
+        word: "human",
+        hint: "predominant species on earth"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to the internet"
+    },
+    {
+        word: "cone",
+        hint: "mathematical shape"
+    },
+    {
+        word: "moon",
+        hint: "earth's sister"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "used to unlock"
+    },
+    {
+        word: "pharaoh",
+        hint: "ruler of an ancient country"
+    },
+    {
+        word: "ruby",
+        hint: "precious stone"
+    },
+    {
+        word: "dubai",
+        hint: "city of skyscrapers"
+    },
+    {
+        word: "photo",
+        hint: "digital representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "longest river"
+    },
+    {
+        word: "rain",
+        hint: "tears from the rumbling sky"
+    },
+]
